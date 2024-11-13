0705f865fc11b9c9114bdf653e576284742c8f95
FatManiac <nicolas.jawh@gmail.com>
2024-11-13 06:51:05 +0100

    add select perso and bonus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,31 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'pauloHead';
   isPauloHeadShowed = false;
+  persos = [
+    {name: 'Paulo', img: 'pauloHead.png'},
+    {name: 'Reflex', img: 'reflexHead.png'},
+    {name: 'Peka', img: 'pekaHead.png'},
+    {name: 'Velfus', img: 'velfusHead.png'},
+    {name: 'Soso', img: 'sosoHead.png'},
+    {name: 'Nico', img: 'nicoHead.png'},
+    {name: 'Axel', img: 'axelHead.png'},
+    {name: 'Bonus', img: 'tomskyeHead.png'},
+  ];
+  persoIndex = 0;
+  isSelectPersoShowed = true;
 
   switchPaulo() {
     console.log('Paulo Head');
     this.isPauloHeadShowed = !this.isPauloHeadShowed;
+  }
+
+  selectPerso(perso: any) {
+    this.persoIndex = this.persos.indexOf(perso);
+    this.isSelectPersoShowed = false;
+    console.log('Selected perso: ', perso);
+  }
+
+  returnToSelectPerso() {
+    this.isSelectPersoShowed = true;
   }
 }
